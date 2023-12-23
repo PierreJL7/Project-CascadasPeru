dd4a3ad90878eedbaa45ec286ec94fa107dbfba4
PierreJL7 <giampiere_15_leon_cib@outlook.com>
2023-12-23 12:25:46 -0500

    prueba

---
+++ js/nav.js
@@ -0,0 +1,184 @@
+
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+/*CARRUSEL */
+if(document.querySelector('#container-slider')){
+  setInterval('fntExecuteSlide("next")',5000);
+}
+//------------------------------ LIST SLIDER -------------------------
+if(document.querySelector('.listslider')){
+  let link = document.querySelectorAll(".listslider li a");
+  link.forEach(function(link) {
+     link.addEventListener('click', function(e){
+        e.preventDefault();
+        let item = this.getAttribute('itlist');
+        let arrItem = item.split("_");
+        fntExecuteSlide(arrItem[1]);
+        return false;
+     });
+   });
+}
+
+function fntExecuteSlide(side){
+   let parentTarget = document.getElementById('slider');
+   let elements = parentTarget.getElementsByTagName('li');
+   let curElement, nextElement;
+
+   for(var i=0; i<elements.length;i++){
+
+       if(elements[i].style.opacity==1){
+           curElement = i;
+           break;
+       }
+   }
+   if(side == 'prev' || side == 'next'){
+
+       if(side=="prev"){
+           nextElement = (curElement == 0)?elements.length -1:curElement -1;
+       }else{
+           nextElement = (curElement == elements.length -1)?0:curElement +1;
+       }
+   }else{
+       nextElement = side;
+       side = (curElement > nextElement)?'prev':'next';
+
+   }
+   //RESALTA LOS PUNTOS
+   let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+   elementSel[curElement].classList.remove("item-select-slid");
+   elementSel[nextElement].classList.add("item-select-slid");
+   elements[curElement].style.opacity=0;
+   elements[curElement].style.zIndex =0;
+   elements[nextElement].style.opacity=1;
+   elements[nextElement].style.zIndex =1;
+}
+
+(function() {
+    "use strict";
+  
+    var carousel = document.getElementsByClassName('carousel')[0],
+        slider = carousel.getElementsByClassName('carousel__slider')[0],
+        items = carousel.getElementsByClassName('carousel__slider__item'),
+        prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+        nextBtn = carousel.getElementsByClassName('carousel__next')[0];
+    
+    var width, height, totalWidth, margin = 20,
+        currIndex = 0,
+        interval, intervalTime = 4000;
+    
+    function init() {
+        resize();
+        move(Math.floor(items.length / 2));
+        bindEvents();
+      
+        timer();
+    }
+    
+    function resize() {
+        width = Math.max(window.innerWidth * .25, 275),
+        height = window.innerHeight * .5,
+        totalWidth = width * items.length;
+      
+        slider.style.width = totalWidth + "px";
+      
+        for(var i = 0; i < items.length; i++) {
+            let item = items[i];
+            item.style.width = (width - (margin * 2)) + "px";
+            item.style.height = height + "px";
+        }
+    }
+    
+    function move(index) {
+      
+        if(index < 1) index = items.length;
+        if(index > items.length) index = 1;
+        currIndex = index;
+      
+        for(var i = 0; i < items.length; i++) {
+            let item = items[i],
+                box = item.getElementsByClassName('item__3d-frame')[0];
+            if(i == (index - 1)) {
+                item.classList.add('carousel__slider__item--active');
+                box.style.transform = "perspective(1200px)"; 
+            } else {
+              item.classList.remove('carousel__slider__item--active');
+                box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
+            }
+        }
+      
+        slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
+    }
+    
+    function timer() {
+        clearInterval(interval);    
+        interval = setInterval(() => {
+          move(++currIndex);
+        }, intervalTime);    
+    }
+    
+    function prev() {
+      move(--currIndex);
+      timer();
+    }
+    
+    function next() {
+      move(++currIndex);    
+      timer();
+    }
+    
+    
+    function bindEvents() {
+        window.onresize = resize;
+        prevBtn.addEventListener('click', () => { prev(); });
+        nextBtn.addEventListener('click', () => { next(); });    
+    }
+  
+  
+  
+  
+    
+    init();
+    
+  })();
